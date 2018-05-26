5892c6471ec1149d8b41ce94bacba93663b8c3c9
Zerui <161558w@student.hci.edu.sg>
2018-05-26 14:29:44 +0800

    Initial Commit

---
+++ SigMa.th/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SigMa.th
+//
+//  Created by Chen Zerui on 26/5/18.
+//  Copyright © 2018 Chen Zerui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
